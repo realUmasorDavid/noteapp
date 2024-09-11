ba46d1b6c98f1e6530698eca979af4649be86b20
Dave <davidumasor18@gmail.com>
2024-09-11 15:17:42 +0100

    mobile upload sizing fix

--- main/note/models.py
+++ main/note/models.py
@@ -7,7 +7,7 @@ class Note(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField(upload_to='note_image')
+    image = models.ImageField(upload_to='note_image', blank=True, null=True)
     created_by = models.ForeignKey(User, blank=True, null=True,  on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     
